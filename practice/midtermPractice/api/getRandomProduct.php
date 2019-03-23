2ec9ebdc7ec18184596e8b0d8d371560d75df0d5
Justin Schroeder <jschroeder@csumb.edu>
2019-03-23 22:11:35 +0000

    mp

--- practice/midtermPractice/api/getRandomProduct.php
+++ practice/midtermPractice/api/getRandomProduct.php
@@ -1,23 +1,9 @@
 <?php
-
-$host = "localhost";
-$dbname = "midterm_practice";
-$username = "root";
-$password = "";
-
-//Connecting to database
-$dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-//Error Handling
-$dbConn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-$sql = "SELECT * FROM mp_product ORDER BY RAND() LIMIT 1";
-$stmt = $dbConn -> prepare($sql);  //$connection MUST be previously initialized
-$stmt->execute();
-$records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
-
-//print_r($records);
-
-echo json_encode($records);
-
+    include '../../../inc/dbConnection.php';
+    $conn = getDatabaseConnection("midterm_practice");
+    $sql = "SELECT * FROM mp_product ORDER BY RAND() LIMIT 1";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($records);
 ?>
