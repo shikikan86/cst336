0477b69cc04cbcb186cbd7d3407705a50e283cb1
Justin Schroeder <jschroeder@csumb.edu>
2019-04-23 20:44:52 +0000

    adding homework 4

---
+++ hw/hw4/api/addReport.php
@@ -0,0 +1,22 @@
+<?php
+
+    include '../../../inc/dbConnection.php';
+    $conn = getDatabaseConnection("weather");
+    
+    $arr = array();
+
+    $arr[":weather"] = $_GET["weather"];
+    $arr[":temp"] = $_GET["temp"];
+    $arr[":humidity"] = $_GET["humidity"];
+    $arr[":wind"] = $_GET["wind"];
+    $arr[":direction"] = $_GET["direction"];
+    
+    $sql = "INSERT INTO weather_report ( `weather`, `temp`, `humidity`, `wind`, `direction` ) VALUES ( :weather, :temp, :humidity,
+:wind, :direction )";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($arr);
+    $records = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo json_encode($records);
+
+?>
