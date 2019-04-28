f320c54b5e74635bf31c8fcfceda53dd377fbb2c
Justin Schroeder <jschroeder@csumb.edu>
2019-04-28 20:17:29 +0000

    adding lab 8

--- labs/lab8/api/favoritesAPI.php
+++ labs/lab8/api/favoritesAPI.php
@@ -1,33 +1,51 @@
 <?php
 
-//receives these parameters: action, url, keyword
- include '../../../inc/dbConnection.php';
- $conn = getDatabaseConnection("c9");
+    // receives these parameters: action, url, keyword
 
- $action = $_GET['action'];
+    include '../../../inc/dbConnection.php';
 
- $np = array();
- 
+    // to get the 2 extra points in the hands on portion of the final exam
+    // 1. add favorites to database
+    // 2. remove favorites from database
+    // 3. display the keyword list from database (use DISTINCT)
+
+    $conn = getDatabaseConnection("c9");
+    
+    $action = $_GET["action"];
+    $arr = array();
+
   switch ($action) {
         
-        case "add":    $sql = "INSERT INTO lab8_pixabay (imageURL, keyword) VALUES (:favorite, :keyword)";
-                       $np[':keyword'] = $_GET['keyword'];
-                       $np[':favorite'] = $_GET['favorite'];
+        case "add":    $arr[":imageURL"] = $_GET["imageURL"];
+            $arr[":keyword"] = $_GET["keyword"];
+            $sql = "INSERT INTO lab8_pixabay (`imageURL`, `keyword`) 
+                    VALUES (:imageURL, :keyword)";
+            
                        break;
-        case "delete":  $sql = "DELETE FROM lab8_pixabay WHERE imageURL = :favorite";
-                        $np[':favorite'] = $_GET['favorite'];
+        case "delete":  $arr[":imageURL"] = $_GET["imageURL"];
+            $sql = "DELETE FROM lab8_pixabay WHERE imageURL = :imageURL";
+
                         break;
         case "keyword": //displays list of unique keywords (hint: use DISTINCT)
+                        $sql = "SELECT DISTINCT(keyword) FROM lab8_pixabay WHERE 1";
                         break;
         case "favorites": //display favorite images based on the keyword 
+                       $arr[":keyword"] = $_GET["keyword"];
+                        $sql = "SELECT imageURL FROM lab8_pixabay WHERE keyword = :keyword";
                         break;
                         
     }//switch
 
 
     $stmt = $conn->prepare($sql);
-    $stmt->execute($np);
+    $stmt->execute($arr);
     
-    
+    //fetching records when using SELECT
+    if ( $action == "keyword" || $action == "favorites") {
+     
+         $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+         
+         echo json_encode($records);
+    }
 
 ?>
